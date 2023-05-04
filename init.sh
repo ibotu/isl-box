793d8abb9613941738408b9098e3d1b13d239bc7
ibotu <ibo.10@live.nl>
2023-05-04 18:13:33 +0200

    man firmware init

---
+++ init.sh
@@ -0,0 +1,15 @@
+isl-box/
+    ├── Doc/                       # Documentation for your project
+    ├── Examples/                  # Example code for your project
+    ├── Lib/                       # Third-party libraries used by your project
+    ├── Src/                       # Source code for your project
+    │   ├── main.c                 # Main firmware code for your project
+    │   ├── usb.c                  # USB-specific code for your project
+    │   └── ...                    # Other source files for your project
+    ├── Inc/                       # Header files for your project
+    │   ├── main.h                 # Header file for main firmware code
+    │   ├── usb.h                  # Header file for USB-specific code
+    │   └── ...                    # Other header files for your project
+    ├── Makefile                   # Makefile for building your project
+    ├── README.md                  # README file for your project
+    └── ...                        # Other files and directories for your project
